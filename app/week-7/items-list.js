511e7b5c9688cf82f13e2f3b7c26a1567274ad59
falcondev <tinhtcp@gmail.com>
2025-10-18 21:59:51 -0600

    Create the week 7 page and added some functionality

---
+++ app/week-7/items-list.js
@@ -0,0 +1,45 @@
+"use client";
+
+import { useState } from "react";
+import Item from "./item";
+
+export default function ItemsList({ itemsData }) {
+
+    const [sortBy, setSortBy] = useState("name");
+
+    const sortName = (itemA, itemB) => {
+        if (itemA.name < itemB.name) return -1;
+        if (itemA.name > itemB.name) return 1;
+        return 0;
+    }
+
+    const sortCategory = (itemA, itemB) => {
+        if (itemA.category < itemB.category) return -1;
+        if (itemA.category > itemB.category) return 1;
+        return 0;
+    }
+
+    const handleSort = [...itemsData].sort((itemA, itemB) => {
+        if (sortBy == "name")
+            return sortName(itemA, itemB);
+        
+        if (sortBy == "category")
+            return sortCategory(itemA, itemB);
+    });
+
+    return (
+    <main>
+        <div className="mt-3 flex gap-2 justify-center">
+            <button onClick={() => setSortBy("name")} className={`p-2 ${sortBy == "name" ? "bg-blue-500" : "bg-gray-600"} text-white rounded-lg cursor-pointer`}>Sort By Name</button>
+            <button onClick={() => setSortBy("category")} className={`p-2 ${sortBy == "category" ? "bg-blue-500" : "bg-gray-600"} text-white rounded-lg cursor-pointer`}>Sort By Category</button>
+        </div>
+
+        <ul>
+            {handleSort.map((item, i) => (
+                <li key={i}> <Item item={item} /> </li>
+            ))}
+            
+        </ul>
+    </main>
+    );
+}
